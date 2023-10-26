b5a5a71a028c8a5450b8a21acfd200405042293a
Sergey Moryakov <sergey@moryakov.com>
2023-10-26 16:35:07 +0300

    practice useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
 import React from "react";
+import { useState } from "react";
 import Button from "./components/buttons/Button";
 // import "./App.css";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const [dayTime, setDayTime] = useState("Noon");
+
+    console.log(useState(0));
+
     function handleButtonOneClick() {
-        alert("Button One Clicked");
+        // Possible simple:
+        // setCounter(counter - 1);
+        // or:
+        setCounter((prevCounter) => {
+            console.log(prevCounter);
+            return prevCounter - 1;
+        });
+        setDayTime("Morning");
     }
 
     function handleButtonTwoClick() {
-        alert("I told you DO NOT try :(((");
+        // Possible simple:
+        // setCounter(counter + 1);
+        // or:
+        setCounter((prevCounter) => {
+            console.log(prevCounter);
+            return prevCounter + 1;
+        });
+        setDayTime("Afternoon");
     }
 
     return (
         <div className="container">
-            <h3>Practicing Placeholder</h3>
-            <Button buttonName={"Try Me"} onClick={handleButtonOneClick} />
-            <Button
-                buttonName={"Don't Try Me"}
-                onClick={handleButtonTwoClick}
-            />
+            <h3>DayTime: {dayTime}</h3>
+            <h3>Counter: {counter}</h3>
+            <div className="manipulators">
+                <Button buttonName={"Deduct"} onClick={handleButtonOneClick} />
+                <Button buttonName={"Add"} onClick={handleButtonTwoClick} />
+            </div>
         </div>
     );
 }
